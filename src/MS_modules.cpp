c6c087f3db9e97f5821f8725c102f9f6e97de433
Phal-anx <31989694+Phal-anx@users.noreply.github.com>
2017-11-22 16:18:37 +0100

    Update MS_modules.cpp

--- src/MS_modules.cpp
+++ src/MS_modules.cpp
@@ -1,14 +1,18 @@
-#include "MS_modules.hpp"
+#include "MS-modules.hpp"
 
 Plugin *plugin;
 
 
 void init(rack::Plugin *p) {
 	plugin = p;		
-	plugin->slug = "MS-Modules";
-	plugin->name = "MS-Modules";
-	plugin->homepageUrl = "https://github.com/Phal-anx/MS-Modules";
-	createModel<RandomSourceWidget>(plugin, "Random Source", "Random Source");
-	createModel<MultWidget>(plugin, "Mult", "Mult");
+	p->slug = "MS-Modules";
+#ifdef VERSION
+	p->version = TOSTRING(VERSION);
+#endif
+	p->website = "https://github.com/Phal-anx/MS-Modules";
 	
+	p->addModel(createModel<RandomSourceV1Widget>("MS-Modules", "Random SourceV1", "Random Source-1", SAMPLE_AND_HOLD_TAG));
+	p->addModel(createModel<RandomSourceV2Widget>("MS-Modules", "Random SourceV2", "Random Source-2", SAMPLE_AND_HOLD_TAG));
+	p->addModel(createModel<MultWidget>("MS-Modules", "Mult", "Mult", MULTIPLE_TAG));
+		
 }
