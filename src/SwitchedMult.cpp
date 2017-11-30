651dc0b1f3df678a17c2a0bf4c8bedd818cb6605
Phal-anx <31989694+Phal-anx@users.noreply.github.com>
2017-11-30 01:30:17 +0100

    Update SwitchedMult.cpp

--- src/SwitchedMult.cpp
+++ src/SwitchedMult.cpp
@@ -1,4 +1,6 @@
-#include "MS-Modules.hpp"
+#include "MSM.hpp"
+
+
 
 struct SwitchedMult : Module {
 	
@@ -31,6 +33,22 @@ struct SwitchedMult : Module {
 		IN_6_INPUT,
 		IN_7_INPUT,
 		IN_8_INPUT,
+		CV_1_INPUT,
+		CV_2_INPUT,
+		CV_3_INPUT,
+		CV_4_INPUT,
+		CV_5_INPUT,
+		CV_6_INPUT,
+		CV_7_INPUT,
+		CV_8_INPUT,
+		CV_11_INPUT,
+		CV_12_INPUT,
+		CV_13_INPUT,
+		CV_14_INPUT,
+		CV_15_INPUT,
+		CV_16_INPUT,
+		CV_17_INPUT,
+		CV_18_INPUT,
 		NUM_INPUTS
 	};
 	
@@ -45,7 +63,7 @@ struct SwitchedMult : Module {
 		OUT_8_OUTPUT,
 		NUM_OUTPUTS
 	};
-	
+		
 	SwitchedMult() : Module(NUM_PARAMS, NUM_INPUTS, NUM_OUTPUTS) {}
 	void step() override;
 
@@ -56,23 +74,24 @@ void SwitchedMult::step() {
 	
 	float IN_1, IN_2, IN_3, IN_4, IN_5, IN_6, IN_7, IN_8, Out_1, Out_2, Out_3, Out_4, Out_5, Out_6, Out_7, Out_8;
 		
-	bool IN_1_Mode = params[IN_1_PARAM].value > 0.0;
-	bool IN_2_Mode = params[IN_2_PARAM].value > 0.0;
-	bool IN_3_Mode = params[IN_3_PARAM].value > 0.0;
-	bool IN_4_Mode = params[IN_4_PARAM].value > 0.0;
-	bool IN_5_Mode = params[IN_5_PARAM].value > 0.0;
-	bool IN_6_Mode = params[IN_6_PARAM].value > 0.0;
-	bool IN_7_Mode = params[IN_7_PARAM].value > 0.0;
-	bool IN_8_Mode = params[IN_8_PARAM].value > 0.0;
+	bool IN_1_Mode = params[IN_1_PARAM].value + inputs[CV_11_INPUT].value > 0.0;
+	bool IN_2_Mode = params[IN_2_PARAM].value + inputs[CV_12_INPUT].value > 0.0;
+	bool IN_3_Mode = params[IN_3_PARAM].value + inputs[CV_13_INPUT].value > 0.0;
+	bool IN_4_Mode = params[IN_4_PARAM].value + inputs[CV_14_INPUT].value > 0.0;
+	bool IN_5_Mode = params[IN_5_PARAM].value + inputs[CV_15_INPUT].value > 0.0;
+	bool IN_6_Mode = params[IN_6_PARAM].value + inputs[CV_16_INPUT].value > 0.0;
+	bool IN_7_Mode = params[IN_7_PARAM].value + inputs[CV_17_INPUT].value > 0.0;
+	bool IN_8_Mode = params[IN_8_PARAM].value + inputs[CV_18_INPUT].value > 0.0;
 	
-	bool MUTE_1 = params[MUTE_1_PARAM].value > 0.0;
-	bool MUTE_2 = params[MUTE_2_PARAM].value > 0.0;
-	bool MUTE_3 = params[MUTE_3_PARAM].value > 0.0;
-	bool MUTE_4 = params[MUTE_4_PARAM].value > 0.0;
-	bool MUTE_5 = params[MUTE_5_PARAM].value > 0.0;
-	bool MUTE_6 = params[MUTE_6_PARAM].value > 0.0;
-	bool MUTE_7 = params[MUTE_7_PARAM].value > 0.0;
-	bool MUTE_8 = params[MUTE_8_PARAM].value > 0.0;
+	bool MUTE_1 = params[MUTE_1_PARAM].value + inputs[CV_1_INPUT].value > 0.0;
+	bool MUTE_2 = params[MUTE_2_PARAM].value + inputs[CV_2_INPUT].value > 0.0;
+	bool MUTE_3 = params[MUTE_3_PARAM].value + inputs[CV_3_INPUT].value > 0.0;
+	bool MUTE_4 = params[MUTE_4_PARAM].value + inputs[CV_4_INPUT].value > 0.0;
+	bool MUTE_5 = params[MUTE_5_PARAM].value + inputs[CV_5_INPUT].value > 0.0;
+	bool MUTE_6 = params[MUTE_6_PARAM].value + inputs[CV_6_INPUT].value > 0.0;
+	bool MUTE_7 = params[MUTE_7_PARAM].value + inputs[CV_7_INPUT].value > 0.0;
+	bool MUTE_8 = params[MUTE_8_PARAM].value + inputs[CV_8_INPUT].value > 0.0;
+	
 	
 	if(MUTE_1) {
 		IN_1 = 0.0;
@@ -103,6 +122,7 @@ void SwitchedMult::step() {
 	}
 	else {
 		IN_5 = inputs[IN_5_INPUT].value;
+
 	}
 	if(MUTE_6) {
 		IN_6 = 0.0;
@@ -125,124 +145,95 @@ void SwitchedMult::step() {
 		
 	if(IN_1_Mode) {
 		if(inputs[IN_1_INPUT].active) {
-		Out_1 = IN_1 + IN_2 + IN_3 + IN_4 + IN_5 + IN_6 + IN_7 + IN_8;
+		Out_1 = IN_1 + IN_2;
 		
 		outputs[OUT_1_OUTPUT].value = Out_1;
 		outputs[OUT_2_OUTPUT].value = Out_1;
-		outputs[OUT_3_OUTPUT].value = Out_1;		
-		outputs[OUT_4_OUTPUT].value = Out_1;
 		}
 	}
+	
 	else {
 		if(inputs[IN_1_INPUT].active) {
-		Out_1 = IN_1 + IN_2 + IN_3 + IN_4 + IN_5 + IN_6 + IN_7 + IN_8;
+		Out_1 = IN_1 + IN_2 + IN_3 + IN_4;
 	
 		outputs[OUT_1_OUTPUT].value = Out_1;
 		outputs[OUT_2_OUTPUT].value = Out_1;
 		outputs[OUT_3_OUTPUT].value = Out_1;		
 		outputs[OUT_4_OUTPUT].value = Out_1;
-		outputs[OUT_5_OUTPUT].value = Out_1;
-		outputs[OUT_6_OUTPUT].value = Out_1;		
-		outputs[OUT_7_OUTPUT].value = Out_1;
-		outputs[OUT_8_OUTPUT].value = Out_1;		
 		}
 	}
-	
 	
 	
 	if(IN_2_Mode) {
 		if(inputs[IN_2_INPUT].active) {
-		Out_2 = IN_1 + IN_2 + IN_3 + IN_4 + IN_5 + IN_6 + IN_7 + IN_8;
+		Out_2 = IN_1 + IN_2;
 		
 		outputs[OUT_1_OUTPUT].value = Out_2;
 		outputs[OUT_2_OUTPUT].value = Out_2;
-		outputs[OUT_3_OUTPUT].value = Out_2;		
-		outputs[OUT_4_OUTPUT].value = Out_2;	
 		}
 	}
+	
 	else {
 		if(inputs[IN_2_INPUT].active) {
-		Out_2 = IN_1 + IN_2 + IN_3 + IN_4 + IN_5 + IN_6 + IN_7 + IN_8;
+		Out_2 = IN_1 + IN_2 + IN_3 + IN_4;
 	
 		outputs[OUT_1_OUTPUT].value = Out_2;
 		outputs[OUT_2_OUTPUT].value = Out_2;
 		outputs[OUT_3_OUTPUT].value = Out_2;		
 		outputs[OUT_4_OUTPUT].value = Out_2;
-		outputs[OUT_5_OUTPUT].value = Out_2;
-		outputs[OUT_6_OUTPUT].value = Out_2;		
-		outputs[OUT_7_OUTPUT].value = Out_2;
-		outputs[OUT_8_OUTPUT].value = Out_2;
 		}
 	}
 	
 	if(IN_3_Mode) {
 		if(inputs[IN_3_INPUT].active) {
-		Out_3 = IN_1 + IN_2 + IN_3 + IN_4 + IN_5 + IN_6 + IN_7 + IN_8;
+		Out_3 = IN_3 + IN_4;
 		
-		outputs[OUT_1_OUTPUT].value = Out_3;
-		outputs[OUT_2_OUTPUT].value = Out_3;
 		outputs[OUT_3_OUTPUT].value = Out_3;		
 		outputs[OUT_4_OUTPUT].value = Out_3;	
 		}
 	}
 	else {
 		if(inputs[IN_3_INPUT].active) {
-		Out_3 = IN_1 + IN_2 + IN_3 + IN_4 + IN_5 + IN_6 + IN_7 + IN_8;
+		Out_3 = IN_1 + IN_2 + IN_3 + IN_4;
 				
 		outputs[OUT_1_OUTPUT].value = Out_3;
 		outputs[OUT_2_OUTPUT].value = Out_3;
 		outputs[OUT_3_OUTPUT].value = Out_3;		
-		outputs[OUT_4_OUTPUT].value = Out_3;
-		outputs[OUT_5_OUTPUT].value = Out_3;
-		outputs[OUT_6_OUTPUT].value = Out_3;		
-		outputs[OUT_7_OUTPUT].value = Out_3;
-		outputs[OUT_8_OUTPUT].value = Out_3;		
+		outputs[OUT_4_OUTPUT].value = Out_3;		
 		}
 	}
 	
 	if(IN_4_Mode) {
 		if(inputs[IN_4_INPUT].active) {
-		Out_4 = IN_1 + IN_2 + IN_3 + IN_4 + IN_5 + IN_6 + IN_7 + IN_8;
+		Out_4 = IN_3 + IN_4;
 		
-		outputs[OUT_1_OUTPUT].value = Out_4;
-		outputs[OUT_2_OUTPUT].value = Out_4;
 		outputs[OUT_3_OUTPUT].value = Out_4;		
 		outputs[OUT_4_OUTPUT].value = Out_4;	
 		}
 	}
 	else {
 		if(inputs[IN_4_INPUT].active) {
-		Out_4 = IN_1 + IN_2 + IN_3 + IN_4 + IN_5 + IN_6 + IN_7 + IN_8;
+		Out_4 = IN_1 + IN_2 + IN_3 + IN_4;
 		
 		outputs[OUT_1_OUTPUT].value = Out_4;
 		outputs[OUT_2_OUTPUT].value = Out_4;
 		outputs[OUT_3_OUTPUT].value = Out_4;		
-		outputs[OUT_4_OUTPUT].value = Out_4;
-		outputs[OUT_5_OUTPUT].value = Out_4;
-		outputs[OUT_6_OUTPUT].value = Out_4;		
-		outputs[OUT_7_OUTPUT].value = Out_4;
-		outputs[OUT_8_OUTPUT].value = Out_4;		
+		outputs[OUT_4_OUTPUT].value = Out_4;		
 		}
 	}
 	
 	if(IN_5_Mode) {
 		if(inputs[IN_5_INPUT].active) {
-		Out_5 = IN_1 + IN_2 + IN_3 + IN_4 + IN_5 + IN_6 + IN_7 + IN_8;
+		Out_5 = IN_5 + IN_6;
 	
 		outputs[OUT_5_OUTPUT].value = Out_5;
-		outputs[OUT_6_OUTPUT].value = Out_5;
-		outputs[OUT_7_OUTPUT].value = Out_5;		
-		outputs[OUT_8_OUTPUT].value = Out_5;	
+		outputs[OUT_6_OUTPUT].value = Out_5;	
 		}
 	}
 	else {
 		if(inputs[IN_5_INPUT].active) {
-		Out_5 = IN_1 + IN_2 + IN_3 + IN_4 + IN_5 + IN_6 + IN_7 + IN_8;
-			
-		outputs[OUT_1_OUTPUT].value = Out_5;
-		outputs[OUT_2_OUTPUT].value = Out_5;
-		outputs[OUT_3_OUTPUT].value = Out_5;		
-		outputs[OUT_4_OUTPUT].value = Out_5;
+		Out_5 = IN_5 + IN_6 + IN_7 + IN_8;
+
 		outputs[OUT_5_OUTPUT].value = Out_5;
 		outputs[OUT_6_OUTPUT].value = Out_5;		
 		outputs[OUT_7_OUTPUT].value = Out_5;
@@ -252,22 +243,16 @@ void SwitchedMult::step() {
 	
 	if(IN_6_Mode) {
 		if(inputs[IN_6_INPUT].active) {
-		Out_6 = IN_1 + IN_2 + IN_3 + IN_4 + IN_5 + IN_6 + IN_7 + IN_8;
+		Out_6 = IN_5 + IN_6;
 			
 		outputs[OUT_5_OUTPUT].value = Out_6;
-		outputs[OUT_6_OUTPUT].value = Out_6;
-		outputs[OUT_7_OUTPUT].value = Out_6;		
-		outputs[OUT_8_OUTPUT].value = Out_6;	
+		outputs[OUT_6_OUTPUT].value = Out_6;	
 		}
 	}
 	else {
 		if(inputs[IN_6_INPUT].active) {
-		Out_6 = IN_1 + IN_2 + IN_3 + IN_4 + IN_5 + IN_6 + IN_7 + IN_8;
-			
-		outputs[OUT_1_OUTPUT].value = Out_6;
-		outputs[OUT_2_OUTPUT].value = Out_6;
-		outputs[OUT_3_OUTPUT].value = Out_6;		
-		outputs[OUT_4_OUTPUT].value = Out_6;
+		Out_6 = IN_5 + IN_6 + IN_7 + IN_8;
+		
 		outputs[OUT_5_OUTPUT].value = Out_6;
 		outputs[OUT_6_OUTPUT].value = Out_6;		
 		outputs[OUT_7_OUTPUT].value = Out_6;
@@ -277,22 +262,16 @@ void SwitchedMult::step() {
 	
 	if(IN_7_Mode) {
 		if(inputs[IN_7_INPUT].active) {
-		Out_7 = IN_1 + IN_2 + IN_3 + IN_4 + IN_5 + IN_6 + IN_7 + IN_8;
-			
-		outputs[OUT_5_OUTPUT].value = Out_7;
-		outputs[OUT_6_OUTPUT].value = Out_7;
+		Out_7 = IN_7 + IN_8;
+
 		outputs[OUT_7_OUTPUT].value = Out_7;		
 		outputs[OUT_8_OUTPUT].value = Out_7;	
 		}
 	}
 	else {
 		if(inputs[IN_7_INPUT].active) {
-		Out_7 = IN_1 + IN_2 + IN_3 + IN_4 + IN_5 + IN_6 + IN_7 + IN_8;
-			
-		outputs[OUT_1_OUTPUT].value = Out_7;
-		outputs[OUT_2_OUTPUT].value = Out_7;
-		outputs[OUT_3_OUTPUT].value = Out_7;		
-		outputs[OUT_4_OUTPUT].value = Out_7;
+		Out_7 = IN_5 + IN_6 + IN_7 + IN_8;
+
 		outputs[OUT_5_OUTPUT].value = Out_7;
 		outputs[OUT_6_OUTPUT].value = Out_7;		
 		outputs[OUT_7_OUTPUT].value = Out_7;
@@ -302,22 +281,16 @@ void SwitchedMult::step() {
 	
 	if(IN_8_Mode) {
 		if(inputs[IN_8_INPUT].active) {
-		Out_8 = IN_1 + IN_2 + IN_3 + IN_4 + IN_5 + IN_6 + IN_7 + IN_8;
+		Out_8 = IN_7 + IN_8;
 		
-		outputs[OUT_5_OUTPUT].value = Out_8;
-		outputs[OUT_6_OUTPUT].value = Out_8;
 		outputs[OUT_7_OUTPUT].value = Out_8;		
 		outputs[OUT_8_OUTPUT].value = Out_8;	
 		}
 	}
 	else {
 		if(inputs[IN_8_INPUT].active) {
-		Out_8 = IN_1 + IN_2 + IN_3 + IN_4 + IN_5 + IN_6 + IN_7 + IN_8;
-				
-		outputs[OUT_1_OUTPUT].value = Out_8;
-		outputs[OUT_2_OUTPUT].value = Out_8;
-		outputs[OUT_3_OUTPUT].value = Out_8;		
-		outputs[OUT_4_OUTPUT].value = Out_8;
+		Out_8 = IN_5 + IN_6 + IN_7 + IN_8;
+
 		outputs[OUT_5_OUTPUT].value = Out_8;
 		outputs[OUT_6_OUTPUT].value = Out_8;		
 		outputs[OUT_7_OUTPUT].value = Out_8;
@@ -332,7 +305,7 @@ SwitchedMultWidget::SwitchedMultWidget()
 {
 	SwitchedMult *module = new SwitchedMult();
     setModule(module);
-    box.size = Vec(150, 380);
+    box.size = Vec(195, 380);
 	
 	{
         SVGPanel *panel = new SVGPanel();
@@ -345,29 +318,30 @@ SwitchedMultWidget::SwitchedMultWidget()
 	
 	addChild(createScrew<ScrewSilver>(Vec(15, 0)));
 	addChild(createScrew<ScrewSilver>(Vec(15, 365)));
-	addChild(createScrew<ScrewSilver>(Vec(120, 0)));
-	addChild(createScrew<ScrewSilver>(Vec(120, 365)));
+	addChild(createScrew<ScrewSilver>(Vec(165, 0)));
+	addChild(createScrew<ScrewSilver>(Vec(165, 365)));
+	//PARAM
+	addParam(createParam<CKSS>(Vec(105, 35 + space), module, SwitchedMult::IN_1_PARAM, 0.0, 1.0, 1.0));
+	addParam(createParam<CKSS>(Vec(105, 70 + space), module, SwitchedMult::IN_2_PARAM, 0.0, 1.0, 1.0));
+	addParam(createParam<CKSS>(Vec(105, 105 + space), module, SwitchedMult::IN_3_PARAM, 0.0, 1.0, 1.0));
+	addParam(createParam<CKSS>(Vec(105, 140 + space), module, SwitchedMult::IN_4_PARAM, 0.0, 1.0, 1.0));
 	
-	addParam(createParam<CKSS>(Vec(84, 35 + space), module, SwitchedMult::IN_1_PARAM, 0.0, 1.0, 1.0));
-	addParam(createParam<CKSS>(Vec(84, 70 + space), module, SwitchedMult::IN_2_PARAM, 0.0, 1.0, 1.0));
-	addParam(createParam<CKSS>(Vec(84, 105 + space), module, SwitchedMult::IN_3_PARAM, 0.0, 1.0, 1.0));
-	addParam(createParam<CKSS>(Vec(84, 140 + space), module, SwitchedMult::IN_4_PARAM, 0.0, 1.0, 1.0));
+	addParam(createParam<CKSS>(Vec(105, 187 + space), module, SwitchedMult::IN_5_PARAM, 0.0, 1.0, 1.0));
+	addParam(createParam<CKSS>(Vec(105, 222 + space), module, SwitchedMult::IN_6_PARAM, 0.0, 1.0, 1.0));
+	addParam(createParam<CKSS>(Vec(105, 257 + space), module, SwitchedMult::IN_7_PARAM, 0.0, 1.0, 1.0));
+	addParam(createParam<CKSS>(Vec(105, 292 + space), module, SwitchedMult::IN_8_PARAM, 0.0, 1.0, 1.0));
 	
-	addParam(createParam<CKSS>(Vec(84, 187 + space), module, SwitchedMult::IN_5_PARAM, 0.0, 1.0, 1.0));
-	addParam(createParam<CKSS>(Vec(84, 222 + space), module, SwitchedMult::IN_6_PARAM, 0.0, 1.0, 1.0));
-	addParam(createParam<CKSS>(Vec(84, 257 + space), module, SwitchedMult::IN_7_PARAM, 0.0, 1.0, 1.0));
-	addParam(createParam<CKSS>(Vec(84, 292 + space), module, SwitchedMult::IN_8_PARAM, 0.0, 1.0, 1.0));
+	addParam(createParam<CKSS>(Vec(75, 35 + space), module, SwitchedMult::MUTE_1_PARAM, 0.0, 1.0, 0.0));
+	addParam(createParam<CKSS>(Vec(75, 70 + space), module, SwitchedMult::MUTE_2_PARAM, 0.0, 1.0, 0.0));
+	addParam(createParam<CKSS>(Vec(75, 105 + space), module, SwitchedMult::MUTE_3_PARAM, 0.0, 1.0, 0.0));
+	addParam(createParam<CKSS>(Vec(75, 140 + space), module, SwitchedMult::MUTE_4_PARAM, 0.0, 1.0, 0.0));
 	
-	addParam(createParam<CKSS>(Vec(53, 35 + space), module, SwitchedMult::MUTE_1_PARAM, 0.0, 1.0, 0.0));
-	addParam(createParam<CKSS>(Vec(53, 70 + space), module, SwitchedMult::MUTE_2_PARAM, 0.0, 1.0, 0.0));
-	addParam(createParam<CKSS>(Vec(53, 105 + space), module, SwitchedMult::MUTE_3_PARAM, 0.0, 1.0, 0.0));
-	addParam(createParam<CKSS>(Vec(53, 140 + space), module, SwitchedMult::MUTE_4_PARAM, 0.0, 1.0, 0.0));
+	addParam(createParam<CKSS>(Vec(75, 187 + space), module, SwitchedMult::MUTE_5_PARAM, 0.0, 1.0, 0.0));
+	addParam(createParam<CKSS>(Vec(75, 222 + space), module, SwitchedMult::MUTE_6_PARAM, 0.0, 1.0, 0.0));
+	addParam(createParam<CKSS>(Vec(75, 257 + space), module, SwitchedMult::MUTE_7_PARAM, 0.0, 1.0, 0.0));
+	addParam(createParam<CKSS>(Vec(75, 292 + space), module, SwitchedMult::MUTE_8_PARAM, 0.0, 1.0, 0.0));
 	
-	addParam(createParam<CKSS>(Vec(53, 187 + space), module, SwitchedMult::MUTE_5_PARAM, 0.0, 1.0, 0.0));
-	addParam(createParam<CKSS>(Vec(53, 222 + space), module, SwitchedMult::MUTE_6_PARAM, 0.0, 1.0, 0.0));
-	addParam(createParam<CKSS>(Vec(53, 257 + space), module, SwitchedMult::MUTE_7_PARAM, 0.0, 1.0, 0.0));
-	addParam(createParam<CKSS>(Vec(53, 292 + space), module, SwitchedMult::MUTE_8_PARAM, 0.0, 1.0, 0.0));
-	
+	//INPUTS
 	addInput(createInput<SilverSixPort>(Vec(10, 35 + space), module, SwitchedMult::IN_1_INPUT));
 	addInput(createInput<SilverSixPort>(Vec(10, 70 + space), module, SwitchedMult::IN_2_INPUT));
 	addInput(createInput<SilverSixPort>(Vec(10, 105 + space), module, SwitchedMult::IN_3_INPUT));
@@ -378,14 +352,36 @@ SwitchedMultWidget::SwitchedMultWidget()
 	addInput(createInput<SilverSixPort>(Vec(10, 257 + space), module, SwitchedMult::IN_7_INPUT));
 	addInput(createInput<SilverSixPort>(Vec(10, 292	+ space), module, SwitchedMult::IN_8_INPUT));
 	
-	addOutput(createOutput<SilverSixPort>(Vec(117, 35 + space), module, SwitchedMult::OUT_1_OUTPUT));
-	addOutput(createOutput<SilverSixPort>(Vec(117, 70 + space), module, SwitchedMult::OUT_2_OUTPUT));
-	addOutput(createOutput<SilverSixPort>(Vec(117, 105 + space), module, SwitchedMult::OUT_3_OUTPUT));
-	addOutput(createOutput<SilverSixPort>(Vec(117, 140 + space), module, SwitchedMult::OUT_4_OUTPUT));
+	//CV
+	addInput(createInput<SilverSixPort>(Vec(40, 35 + space), module, SwitchedMult::CV_1_INPUT));
+	addInput(createInput<SilverSixPort>(Vec(40, 70 + space), module, SwitchedMult::CV_2_INPUT));
+	addInput(createInput<SilverSixPort>(Vec(40, 105 + space), module, SwitchedMult::CV_3_INPUT));
+	addInput(createInput<SilverSixPort>(Vec(40, 140 + space), module, SwitchedMult::CV_4_INPUT));
 	
-	addOutput(createOutput<SilverSixPort>(Vec(117, 187 + space), module, SwitchedMult::OUT_5_OUTPUT));
-	addOutput(createOutput<SilverSixPort>(Vec(117, 222 + space), module, SwitchedMult::OUT_6_OUTPUT));
-	addOutput(createOutput<SilverSixPort>(Vec(117, 257 + space), module, SwitchedMult::OUT_7_OUTPUT));
-	addOutput(createOutput<SilverSixPort>(Vec(117, 292 + space), module, SwitchedMult::OUT_8_OUTPUT));
+	addInput(createInput<SilverSixPort>(Vec(40, 187 + space), module, SwitchedMult::CV_5_INPUT));
+	addInput(createInput<SilverSixPort>(Vec(40, 222 + space), module, SwitchedMult::CV_6_INPUT));
+	addInput(createInput<SilverSixPort>(Vec(40, 257 + space), module, SwitchedMult::CV_7_INPUT));
+	addInput(createInput<SilverSixPort>(Vec(40, 292 + space), module, SwitchedMult::CV_8_INPUT));
+	
+	addInput(createInput<SilverSixPort>(Vec(130, 35 + space), module, SwitchedMult::CV_11_INPUT));
+	addInput(createInput<SilverSixPort>(Vec(130, 70 + space), module, SwitchedMult::CV_12_INPUT));
+	addInput(createInput<SilverSixPort>(Vec(130, 105 + space), module, SwitchedMult::CV_13_INPUT));
+	addInput(createInput<SilverSixPort>(Vec(130, 140 + space), module, SwitchedMult::CV_14_INPUT));
+	
+	addInput(createInput<SilverSixPort>(Vec(130, 187 + space), module, SwitchedMult::CV_15_INPUT));
+	addInput(createInput<SilverSixPort>(Vec(130, 222 + space), module, SwitchedMult::CV_16_INPUT));
+	addInput(createInput<SilverSixPort>(Vec(130, 257 + space), module, SwitchedMult::CV_17_INPUT));
+	addInput(createInput<SilverSixPort>(Vec(130, 292 + space), module, SwitchedMult::CV_18_INPUT));
+	
+	//OUTPUTS
+	addOutput(createOutput<SilverSixPort>(Vec(162, 35 + space), module, SwitchedMult::OUT_1_OUTPUT));
+	addOutput(createOutput<SilverSixPort>(Vec(162, 70 + space), module, SwitchedMult::OUT_2_OUTPUT));
+	addOutput(createOutput<SilverSixPort>(Vec(162, 105 + space), module, SwitchedMult::OUT_3_OUTPUT));
+	addOutput(createOutput<SilverSixPort>(Vec(162, 140 + space), module, SwitchedMult::OUT_4_OUTPUT));
+	
+	addOutput(createOutput<SilverSixPort>(Vec(162, 187 + space), module, SwitchedMult::OUT_5_OUTPUT));
+	addOutput(createOutput<SilverSixPort>(Vec(162, 222 + space), module, SwitchedMult::OUT_6_OUTPUT));
+	addOutput(createOutput<SilverSixPort>(Vec(162, 257 + space), module, SwitchedMult::OUT_7_OUTPUT));
+	addOutput(createOutput<SilverSixPort>(Vec(162, 292 + space), module, SwitchedMult::OUT_8_OUTPUT));
 	
 };
